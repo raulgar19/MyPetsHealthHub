182dd81b3ea2948727e90474884c1e68af02d40b
raulgar19 <raulgamu2003@gmail.com>
2025-01-27 20:29:56 +0100

    Login hecho con parámetros en la navegacion

--- components/screens/HomeScreen.jsx
+++ components/screens/HomeScreen.jsx
@@ -29,9 +29,7 @@ const HomeScreen = () => {
       <ScrollView contentContainerStyle={styles.container}>
         <View style={styles.innerContainer}>
           <Text style={styles.title}>Bienvenid@ de nuevo, "Nombre"</Text>
-
-          <Text style={styles.categoryTitle}>Eres precioso HIJO DE LA GRAN PUTA</Text>
-          <Text style={color}>Gestión</Text>
+          <Text style={styles.categoryTitle}>Gestión</Text>
           <View style={styles.menuContainer}>
             <Link asChild href={"/petsManagement"}>
               <Pressable style={styles.menuItem}>
